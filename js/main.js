9d360e42a1f4aa466bc332b47f696a96a0ba6a84
Elsayed-Elpna <111093103+Elsayed-Elpna@users.noreply.github.com>
2024-01-06 18:49:08 +0200

    add section n-4

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:0,
-  nav:true,
-  dotsEach:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:2
-      },
-      1000:{
-          items:3
-      }
-  }
-})
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 15,
+  nav: true,
+  dotsEach: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
